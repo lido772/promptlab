6aaf1c9461986eed7bff8d71df00212106802bb7
Walid Ben Miled <lido772@gmail.com>
2026-03-09 00:18:04 +0100

    Add Vite/Tailwind build

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+﻿module.exports = {
+  content: [
+    './index.html',
+    './app.js',
+    './animations.js',
+    './promptAnalyzer.js',
+    './llmEngine.js',
+    './modelSelector.js',
+    './i18n.js',
+    './js/**/*.js',
+    './css/**/*.css'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
